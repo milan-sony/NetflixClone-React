385c39667e916214d7ac43633216bdd2663f459c
Milan Sony <milansonyofficial@gmail.com>
2024-11-05 11:27:13 +0530

    style: card style margin top removed

--- src/components/RowCards/RowCards.jsx
+++ src/components/RowCards/RowCards.jsx
@@ -4,7 +4,7 @@ import './RowCards.css'
 function RowCards() {
     return (
         <div className='container-fluid'>
-            <div className='container mt-3 mb-5'>
+            <div className='container mb-5'>
                     <h1 className='mb-5 head-text'>Movie list</h1>
                 <div className='row'>
                     <div className='col-lg-3'>
