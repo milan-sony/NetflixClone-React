a098c423eaf590c07555a58140b25ceff9222d6f
Milan Sony <milansonyofficial@gmail.com>
2024-11-05 11:26:37 +0530

    style: navbar style modified

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ import '../Navbar/Navbar.css'
 function Navbar() {
     return (
         <>
-            <nav class="navbar nav-bar">
-                <div class="container-fluid">
-                    <a class="navbar-brand"><img className='logo' src={netflixLogo} alt="Netflix logo" /></a>
-                    <div class="d-flex" role="search">
-                        <i class="bi bi-person-circle avatar"></i>
+            <nav className="navbar nav-bar">
+                <div className="container-fluid">
+                    <a className="navbar-brand"><img className='logo' src={netflixLogo} alt="Netflix logo" /></a>
+                    <div className="d-flex" role="search">
+                        <i className="bi bi-person-circle avatar"></i>
                     </div>
                 </div>
             </nav>
